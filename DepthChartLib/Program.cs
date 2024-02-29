d43e392218b787ba296c881476494fb1d14f6f3a
Dominic <dominic@bepeerless.co>
2024-02-29 11:34:51 +0100

    Add project files.

---
+++ DepthChartLib/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DepthChartLib
+{
+    public enum Sports
+    {
+        NFL,
+        MLB
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+
+    public interface IDepthChart
+    {
+        void AddPlayerToDepthChart(string position, Player player, int? depth = null);
+        void RemovePlayerFromDepthChart(string position, Player player);
+        IList<Player> GetFullDepthChart();
+        IList<Player> GetPlayersUnderPlayerInDepthChart(string position, Player player);
+    }
+}
